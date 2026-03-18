0482f8bcf34799f4f754dbe82875ee3373a061da
Henrique Schenkel Araujo <henrique16.schenkel@gmail.com>
2026-03-18 15:50:34 -0300

    substitui buttons por ionbutton com icones

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,50 +2,58 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Lista de Tarefas</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
  
     <ion-content :fullscreen="true">
-
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Home</ion-title>
+        </ion-toolbar>
+      </ion-header>
+ 
       <div id="container">
-        <strong>Bem-vindo ao App de Tarefas</strong>
-
-        <p>Organize suas atividades do dia de forma simples.</p>
-
-        <ion-button @click="router.push('/tarefas')">
-          Ir para tarefas
-        </ion-button>
-
+        <strong>Ready to create an app?</strong>
+        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-button @click="router.push('/tarefas')"> ir para tarefas </ion-button>
       </div>
-
     </ion-content>
   </ion-page>
 </template>
  
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/vue'
-import router from '@/router'
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, } from '@ionic/vue';
+import router from '@/router';
 </script>
  
 <style scoped>
 #container {
   text-align: center;
-
+ 
   position: absolute;
   left: 0;
   right: 0;
   top: 50%;
   transform: translateY(-50%);
 }
-
+ 
 #container strong {
-  font-size: 22px;
+  font-size: 20px;
+  line-height: 26px;
 }
-
+ 
 #container p {
   font-size: 16px;
+  line-height: 22px;
+ 
   color: #8c8c8c;
-  margin: 10px 0 20px 0;
+ 
+  margin: 0;
+}
+ 
+#container a {
+  text-decoration: none;
 }
 </style>
+ 
--- src/views/TarefasPage.vue
+++ src/views/TarefasPage.vue
@@ -3,51 +3,53 @@
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Tarefas</ion-title>
-        <ion-button @click="router.push('/home')">
-          Ir para tarefas
-        </ion-button>
+        <ion-button @click="router.push('/Home')"> ir para Home </ion-button>
       </ion-toolbar>
     </ion-header>
-
+ 
     <ion-content class="ion-padding">
-
+ 
       <ion-input
         v-model="novaTarefa"
         placeholder="Digite uma tarefa"
       ></ion-input>
-
+ 
       <!-- BOTÃO -->
-      <ion-button expand="block" @click="adicionarTarefa">
-        Adicionar
+      <ion-button expand="block" color="primary" @click="adicionarTarefa">
+          <ion-icon :icon="addOutline" slot="start"></ion-icon>
+        Adicionar Tarefa
       </ion-button>
-
+ 
       <!-- LISTA -->
       <ion-list>
-
+ 
         <ion-item v-for="(tarefa, index) in tarefas" :key="index">
           <ion-label>
             {{ tarefa }}
           </ion-label>
-
-          <ion-button color="danger" @click="removerTarefa(index)">
-            Remover
+ 
+          <ion-button fill="clear" color="danger" slot="end" @click="removerTarefa(index)">
+           <ion-icon :icon="trash"></ion-icon>
           </ion-button>
         </ion-item>
-
+ 
       </ion-list>
-
+ 
       <p v-if="tarefas.length === 0">
-        Nenhuma tarefa cadastrada. Adicione a primeira!
+        Nenhuma tarefa cadastrada.
       </p>
-
+ 
     </ion-content>
   </ion-page>
 </template>
-
+ 
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import { trash } from 'ionicons/icons'
+import { addOutline } from 'ionicons/icons'
+ 
 import {
+  IonIcon,
   IonContent,
   IonHeader,
   IonPage,
@@ -59,20 +61,20 @@ import {
   IonItem,
   IonLabel
 } from '@ionic/vue'
-import router from '@/router'
-
+import router from '@/router';
+ 
 const novaTarefa = ref('')
 const tarefas = ref<string[]>([])
-
+ 
 function adicionarTarefa() {
-
+ 
   if (novaTarefa.value.trim() === '') return
-
+ 
   tarefas.value.push(novaTarefa.value)
-
+ 
   novaTarefa.value = ''
 }
-
+ 
 function removerTarefa(index: number) {
   tarefas.value.splice(index, 1)
 }
