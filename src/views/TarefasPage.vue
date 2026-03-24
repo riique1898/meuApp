148bf5c69c0c2a1215d921472894d230776e889e
Henrique Schenkel Araujo <henrique16.schenkel@gmail.com>
2026-03-24 16:00:13 -0300

    card tarefa com props e emits usar usetarefas e cardtarefa na tarefapage watch para alertar quando todas as tarefas forem concluidas

--- src/views/TarefasPage.vue
+++ src/views/TarefasPage.vue
@@ -1,52 +1,7 @@
-<template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Tarefas</ion-title>
-        <ion-button @click="router.push('/Home')"> ir para Home </ion-button>
-      </ion-toolbar>
-    </ion-header>
- 
-    <ion-content class="ion-padding">
- 
-      <ion-input
-        v-model="novaTarefa"
-        placeholder="Digite uma tarefa"
-      ></ion-input>
- 
-      <!-- BOTÃO -->
-      <ion-button expand="block" color="primary" @click="adicionarTarefa">
-          <ion-icon :icon="addOutline" slot="start"></ion-icon>
-        Adicionar Tarefa
-      </ion-button>
- 
-      <!-- LISTA -->
-      <ion-list>
- 
-        <ion-item v-for="(tarefa, index) in tarefas" :key="index">
-          <ion-label>
-            {{ tarefa }}
-          </ion-label>
- 
-          <ion-button fill="clear" color="danger" slot="end" @click="removerTarefa(index)">
-           <ion-icon :icon="trash"></ion-icon>
-          </ion-button>
-        </ion-item>
- 
-      </ion-list>
- 
-      <p v-if="tarefas.length === 0">
-        Nenhuma tarefa cadastrada.
-      </p>
- 
-    </ion-content>
-  </ion-page>
-</template>
- 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { trash } from 'ionicons/icons'
+import { ref, watch } from 'vue'
 import { addOutline } from 'ionicons/icons'
+import router from '@/router'
  
 import {
   IonIcon,
@@ -56,26 +11,66 @@ import {
   IonTitle,
   IonToolbar,
   IonInput,
-  IonButton,
-  IonList,
-  IonItem,
-  IonLabel
+  IonButton
 } from '@ionic/vue'
-import router from '@/router';
+ 
+import { useTarefas } from '../composables/useTarefas'
+import CardTarefa from '../components/CardTarefa.vue'
+ 
+const { tarefas, adicionar, remover, concluir, totalPendentes } = useTarefas()
  
 const novaTarefa = ref('')
-const tarefas = ref<string[]>([])
  
 function adicionarTarefa() {
- 
-  if (novaTarefa.value.trim() === '') return
- 
-  tarefas.value.push(novaTarefa.value)
- 
+  adicionar(novaTarefa.value)
   novaTarefa.value = ''
 }
  
-function removerTarefa(index: number) {
-  tarefas.value.splice(index, 1)
-}
+watch(totalPendentes, (valor) => {
+  if (valor === 0 && tarefas.value.length > 0) {
+    alert('🎉 Parabéns! Todas as tarefas foram concluídas!')
+  }
+})
 </script>
+ 
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Tarefas</ion-title>
+        <ion-button @click="router.push('/Home')">
+          ir para Home
+        </ion-button>
+      </ion-toolbar>
+    </ion-header>
+ 
+    <ion-content class="ion-padding">
+ 
+      <ion-input
+        label="Nome da Tarefa"
+        label-placement="floating"
+        v-model="novaTarefa"
+        :clear-input="true"
+        placeholder="Digite uma tarefa"
+      />
+ 
+      <ion-button expand="block" color="primary" @click="adicionarTarefa">
+        <ion-icon :icon="addOutline" slot="start" />
+        Adicionar Tarefa
+      </ion-button>
+ 
+      <CardTarefa
+        v-for="tarefa in tarefas"
+        :key="tarefa.id"
+        :tarefa="tarefa"
+        @remover="remover"
+        @concluir="concluir"
+      />
+ 
+      <p v-if="tarefas.length === 0">
+        Nenhuma tarefa cadastrada.
+      </p>
+ 
+    </ion-content>
+  </ion-page>
+</template>
